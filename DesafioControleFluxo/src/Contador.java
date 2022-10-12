2663126a754c9ab2e4c32a3514efd5c5a59783b9
LLOlaBB <lauracbarros@hotmail.com>
2022-10-12 19:51:52 -0300

    Desafio de Fluxo completo

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+		try (Scanner leitor = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro parâmetro");
+            int numeroUm = leitor.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int numeroDois = leitor.nextInt();
+            
+            try {
+            	contar(numeroUm, numeroDois);
+            
+            }catch (ParametrosInvalidosException e) {
+            	System.out.println("O primeiro numero é maior que o segundo");
+            }
+        }
+		
+	}
+	static void contar(int numeroUm, int numeroDois ) throws ParametrosInvalidosException {
+		if(numeroUm > numeroDois)
+            throw new ParametrosInvalidosException();
+        else{
+            int contador = numeroDois - numeroUm;
+            for(int i =0; i<= contador; i++){
+                System.out.println("Imprimindo o numero "+ i);
+            }
+        }
+	}
+}
